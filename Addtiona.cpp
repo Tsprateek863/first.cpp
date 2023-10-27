bcabdbe10a289d673dafe35edadc26c26ab56196
Pratik Tiwari <149098481+Tsprateek863@users.noreply.github.com>
2023-10-27 17:53:35 +0530

    Create Addtiona.cpp
    Just trying to GitHub

---
+++ Addtiona.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a, b,c;
+  cout<<"Enter value of a & b";
+  cin>>a>>b;
+  c= a+b;
+  cout<<"Addtion of two values :"<<c<<endl;
+return 0;
+}
